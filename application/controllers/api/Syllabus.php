82b924e1973bc5d600e1ac4bf4648b6ce889766b
NEX3VWN <Pitney@8>
2019-06-14 22:11:21 +0530

    Auto Alignment by command Alt+Shift+F

--- application/controllers/api/Syllabus.php
+++ application/controllers/api/Syllabus.php
@@ -1,41 +1,45 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 require APPPATH . 'libraries/REST_Controller.php';
 
-class Syllabus extends REST_Controller {
+class Syllabus extends REST_Controller
+{
 
-  public function __construct() {
+  public function __construct()
+  {
     parent::__construct();
     $this->load->database();
     $this->load->model('SyllabusModel');
   }
 
-  public function index_post() {
-    if(isTheseParametersAvailable(array('class_id'))){
+  public function index_post()
+  {
+    if (isTheseParametersAvailable(array('class_id'))) {
       $response = array();
       $class_id = $this->input->post('class_id');
       $response = $this->get_syllabusByClass($class_id); // getting syllabus by class
       $httpStatus = REST_Controller::HTTP_OK;
-    }else{
+    } else {
       $response['error'] = true;
       $response['message'] = "Parameters not found";
       $httpStatus = REST_Controller::HTTP_BAD_REQUEST;
     }
-    
-    $this->response($response, $httpStatus);
-    } 
 
-    private function get_syllabusByClass($class_id){
-        $data = $this->SyllabusModel->get_syllabusByClass($class_id);
-        if(!$data==false) {            
-            $response['error'] = false;
-            $response['message'] = "Syllabus fetched successfully";
-            $response['timetable'] = $data;
-        } else{
-            $response['error'] = true;
-            $response['message'] = "No data found";
-        }   
-        return $response;
-    } 
+    $this->response($response, $httpStatus);
+  }
+
+  private function get_syllabusByClass($class_id)
+  {
+    $data = $this->SyllabusModel->get_syllabusByClass($class_id);
+    if (!$data == false) {
+      $response['error'] = false;
+      $response['message'] = "Syllabus fetched successfully";
+      $response['timetable'] = $data;
+    } else {
+      $response['error'] = true;
+      $response['message'] = "No data found";
+    }
+    return $response;
+  }
 }
