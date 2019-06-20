c83a19b03148abc127383562c0b238915da9c5df
NEX3VWN <Pitney@8>
2019-06-20 21:50:47 +0530

    DownloadReceiver Downloading status is added and handled other cases.

--- app/src/main/java/com/pb/apszone/view/receiver/DownloadBroadcastReceiver.java
+++ app/src/main/java/com/pb/apszone/view/receiver/DownloadBroadcastReceiver.java
@@ -4,17 +4,95 @@ import android.app.DownloadManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.widget.Toast;
 
+import static android.content.Context.DOWNLOAD_SERVICE;
 import static com.pb.apszone.utils.AppConstants.KEY_DOWNLOAD_ID;
 
 public class DownloadBroadcastReceiver extends BroadcastReceiver {
+    DownloadManager downloadManager;
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        downloadManager = (DownloadManager) context.getSystemService(DOWNLOAD_SERVICE);
         long id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
         //Checking if the received broadcast is for our enqueued download by matching download id
         if (KEY_DOWNLOAD_ID == id) {
-            Toast.makeText(context, "Download Completed", Toast.LENGTH_SHORT).show();
+            DownloadManager.Query query = new DownloadManager.Query();
+            query.setFilterById(id);
+            Cursor cursor = downloadManager.query(query);
+
+            if (cursor.moveToFirst()) {
+                int columnIndex = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS);
+                int status = cursor.getInt(columnIndex);
+                int columnReason = cursor.getColumnIndex(DownloadManager.COLUMN_REASON);
+                int reason = cursor.getInt(columnReason);
+
+                switch (status) {
+                    case DownloadManager.STATUS_FAILED:
+                        String failedReason = "";
+                        switch (reason) {
+                            case DownloadManager.ERROR_CANNOT_RESUME:
+                                failedReason = "ERROR_CANNOT_RESUME";
+                                break;
+                            case DownloadManager.ERROR_DEVICE_NOT_FOUND:
+                                failedReason = "ERROR_DEVICE_NOT_FOUND";
+                                break;
+                            case DownloadManager.ERROR_FILE_ALREADY_EXISTS:
+                                failedReason = "ERROR_FILE_ALREADY_EXISTS";
+                                break;
+                            case DownloadManager.ERROR_FILE_ERROR:
+                                failedReason = "ERROR_FILE_ERROR";
+                                break;
+                            case DownloadManager.ERROR_HTTP_DATA_ERROR:
+                                failedReason = "ERROR_HTTP_DATA_ERROR";
+                                break;
+                            case DownloadManager.ERROR_INSUFFICIENT_SPACE:
+                                failedReason = "ERROR_INSUFFICIENT_SPACE";
+                                break;
+                            case DownloadManager.ERROR_TOO_MANY_REDIRECTS:
+                                failedReason = "ERROR_TOO_MANY_REDIRECTS";
+                                break;
+                            case DownloadManager.ERROR_UNHANDLED_HTTP_CODE:
+                                failedReason = "ERROR_UNHANDLED_HTTP_CODE";
+                                break;
+                            case DownloadManager.ERROR_UNKNOWN:
+                                failedReason = "ERROR_UNKNOWN";
+                                break;
+                        }
+                        Toast.makeText(context, "Download Failed. " + failedReason, Toast.LENGTH_LONG).show();
+                        break;
+                    case DownloadManager.STATUS_PAUSED:
+                        String pausedReason = "";
+
+                        switch (reason) {
+                            case DownloadManager.PAUSED_QUEUED_FOR_WIFI:
+                                pausedReason = "PAUSED_QUEUED_FOR_WIFI";
+                                break;
+                            case DownloadManager.PAUSED_UNKNOWN:
+                                pausedReason = "PAUSED_UNKNOWN";
+                                break;
+                            case DownloadManager.PAUSED_WAITING_FOR_NETWORK:
+                                pausedReason = "PAUSED_WAITING_FOR_NETWORK";
+                                break;
+                            case DownloadManager.PAUSED_WAITING_TO_RETRY:
+                                pausedReason = "PAUSED_WAITING_TO_RETRY";
+                                break;
+                        }
+                        Toast.makeText(context, "Download Paused " + pausedReason, Toast.LENGTH_LONG).show();
+                        break;
+                    case DownloadManager.STATUS_PENDING:
+                        Toast.makeText(context, "PENDING", Toast.LENGTH_LONG).show();
+                        break;
+                    case DownloadManager.STATUS_RUNNING:
+                        Toast.makeText(context, "RUNNING", Toast.LENGTH_LONG).show();
+                        break;
+                    case DownloadManager.STATUS_SUCCESSFUL:
+                        Toast.makeText(context, "Download Complete", Toast.LENGTH_LONG).show();
+                        break;
+                }
+            }
         }
     }
 }
