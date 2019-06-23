ccdb210cdfc1a626ff50b136176c6deec2517463
NEX3VWN <Pitney@8>
2019-06-24 00:06:11 +0530

    Created Notification Api for Android Fetch

--- application/models/NotificationModel.php
+++ application/models/NotificationModel.php
@@ -7,8 +7,25 @@ class NotificationModel extends CI_Model
         $this->load->database();
     }
 
+    /* Query to add payload data*/
     public function add_notification($array)
     {
+        date_default_timezone_set("Asia/Calcutta");
+        $array['date'] = date("Y-m-d H:i:sa");
         return $this->db->insert('inbox', $array);
+    }
+
+    /* Query for getting all notification data*/
+    public function get_notification()
+    {
+        $query = $this->db->where('topic', 'global')
+            ->order_by('date', 'DESC')
+            ->get('inbox');
+
+        if ($query->num_rows()) {
+            return $query->result();
+        } else {
+            return FALSE;
+        }
     }
 }
