7b181e31567c5c7008e42af5fc19cd2ebbbe5f90
NEX3VWN <Pitney@8>
2019-06-27 01:07:10 +0530

    Timetable Api for to get teacher's data by day is integrated

--- application/controllers/api/TimeTable.php
+++ application/controllers/api/TimeTable.php
@@ -22,6 +22,11 @@ class TimeTable extends REST_Controller {
             $class_id = $this->input->post('class_id');
             $response = $this->get_timeTableByWeek($class_id);
             $httpStatus = REST_Controller::HTTP_OK;
+        } elseif(isTheseParametersAvailable(array('teacher_id', 'today'))) {
+            $teacher_id = $this->input->post('teacher_id');
+            $today = $this->input->post('today');
+            $response = $this->get_TeacherTimeTableByDay($teacher_id, $today);
+            $httpStatus = REST_Controller::HTTP_OK;
         } else {
             $response['error'] = true;
             $response['message'] = "Parameters not found";
@@ -52,6 +57,31 @@ class TimeTable extends REST_Controller {
             $response['error'] = true;
             $response['message'] = "No data found";
         }   
+        return $response;
+    }
+
+    /* get teacher's timetable function @param teacher_id and today*/
+    public function get_TeacherTimeTableByDay($teacher_id, $today) {
+        $data = $this->TimeTableModel->get_TeacherTimeTableByDay($teacher_id, $today); // getting Timetable by day
+        if(!$data==false) {
+            foreach ($data as $key => $field) {
+                if ($field->class_id) {
+                    $data[$key]->class_id = getClassDetails($field->class_id); // getting class details and adding it into data array
+                }
+                if ($field->subject_id) {
+                    $data[$key]->subject_id = getSubjectDetails($field->subject_id); // getting subject details and adding it into data array
+                }
+                if ($field->teacher_id) {
+                    $data[$key]->teacher_id = getTeacherDetails($field->teacher_id); // getting teacher details and adding it into data array
+                }
+            }
+            $response['error'] = false;
+            $response['message'] = "Teacher timetable fetched successfully";
+            $response['timetable'] = $data;
+        } else{
+            $response['error'] = true;
+            $response['message'] = "No data found";
+        }
         return $response;
     }
 
--- application/models/TimeTableModel.php
+++ application/models/TimeTableModel.php
@@ -8,10 +8,22 @@ class TimeTableModel extends CI_model {
         $this->load->database();
     }
 
-	/* Query for getting timetable by day */
+	/* Query for getting timetable by day for students */
     public function get_timeTableByDay($class_id,$today) {
         $query = $this->db->where(['class_id'=>$class_id, 'day'=>$today])
     				  ->from('timetable')	
+                      ->get();
+        if($query->num_rows()){
+            return $query->result();
+        }else{
+            return FALSE;
+        }
+    }
+
+    /* Query for getting timetable by day for teachers */
+    public function get_TeacherTimeTableByDay($teacher_id, $today) {
+        $query = $this->db->where(['teacher_id'=>$teacher_id, 'day'=>$today])
+    				  ->from('timetable')
                       ->get();
         if($query->num_rows()){
             return $query->result();
