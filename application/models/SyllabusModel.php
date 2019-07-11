38ee2bdb4fe739eb68497667931005e549c34865
NEX3VWN <Pitney@8>
2019-07-12 01:04:01 +0530

    Add and check for syllabus api integrated

--- application/models/SyllabusModel.php
+++ application/models/SyllabusModel.php
@@ -1,26 +1,48 @@
 <?php
 
-class SyllabusModel extends CI_model {
+class SyllabusModel extends CI_model
+{
 
-	public function __construct() 
+    public function __construct()
     {
-        parent::__construct(); 
+        parent::__construct();
         $this->load->database();
     }
 
-	/* Query for getting syllabus by class*/
-    public function get_syllabusByClass($class_id) {
-        $query = $this->db->where(['class_id'=>$class_id])
-    				  ->from('subject')	
-                      ->get();
-        if($query->num_rows()){
+    /* Query for getting syllabus by class*/
+    public function get_syllabusByClass($class_id)
+    {
+        $query = $this->db->where(['class_id' => $class_id])
+            ->from('subject')
+            ->get();
+        if ($query->num_rows()) {
             return $query->result();
-        }else{
+        } else {
             return FALSE;
         }
-    }   
+    }
 
+    /* Teacher - Query to check if syllabus is already available by subject_id*/
+    public function isSyllabusAvailable($subject_id)
+    {
+        $sql = 'SELECT syllabus FROM subject WHERE id = "' . $subject_id . '"';
+        $query = $this->db->query($sql);
+        if ($query->num_rows()) {
+            return $query->row();
+        } else {
+            return NULL;
+        }
+    }
+
+    /* Teacher - Query to add syllabus data */
+    public function add_Syllabus($subject_id, $path)
+    {
+        $sql = 'UPDATE subject SET syllabus = "' . $path . '" WHERE id = "' . $subject_id . '"';
+        $query = $this->db->query($sql);
+        if ($query) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
 }
-
-
-?>
