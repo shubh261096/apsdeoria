68cf7ec7040950ca07740696c6b0ceff909d520e
NEX3VWN <Pitney@8>
2019-07-11 13:02:20 +0530

    Logging Inteceptor is added to log the request and response and GsonConverterfactory to send reuqest as body.

--- app/src/main/java/com/pb/apszone/service/rest/ApiClient.java
+++ app/src/main/java/com/pb/apszone/service/rest/ApiClient.java
@@ -1,5 +1,10 @@
 package com.pb.apszone.service.rest;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -10,10 +15,15 @@ public class ApiClient {
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.level(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+        Gson gson = new GsonBuilder().setLenient().create();
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
                     .build();
         }
         return retrofit;
