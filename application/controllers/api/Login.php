644422dbe14b85b1ee0750bc73ecae80b975cf99
NEX3VWN <Pitney@8>
2019-07-19 02:27:20 +0530

    1. Email Concept from login is removed 2. Hack for teacher user-type in login model since Dashboard and other componenets are bind to user-type parent

--- application/controllers/api/Login.php
+++ application/controllers/api/Login.php
@@ -1,51 +1,52 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 require APPPATH . 'libraries/REST_Controller.php';
 
-class Login extends REST_Controller {
+class Login extends REST_Controller
+{
 
-  public function __construct() {
+  public function __construct()
+  {
     parent::__construct();
     $this->load->database();
     $this->load->model('LoginModel');
   }
 
-  public function index_post() {
-    if(isTheseParametersAvailable(array('email','password'))){
-      $response = array();     
-      $email = $this->input->post('email');
+  public function index_post()
+  {
+    if (isTheseParametersAvailable(array('id', 'password'))) {
+      $response = array();
+      $id = $this->input->post('id');
       $password = $this->input->post('password');
-      $data['row'] = $this->LoginModel->validate_login($email, $password); // validating login
-      if (!$data['row']==false) {
-        $id = $this->LoginModel->get_uniqueId($email, $data['row']->type); // getting userId if status is active or 1
-        if(!$id==false) {
+      $data['row'] = $this->LoginModel->validate_login($id, $password); // validating login
+      if (!$data['row'] == false) {
+        $status = $this->LoginModel->get_userStatus($id, $data['row']->type); // getting if status is active or not
+        if (!$status == false) {
           $user = array(
-            'id'=>$id,
-            'email'=>$email, 
-            'type'=>$data['row']->type
-            );
-            $response['error'] = false;
-            $response['message'] = "Login Successfull";
-            $response['user'] = $user;
-            $httpStatus = REST_Controller::HTTP_OK;
-        }else{
+            'id' => $id,
+            'type' => $data['row']->type
+          );
+          $response['error'] = false;
+          $response['message'] = "Login Successfull";
+          $response['user'] = $user;
+          $httpStatus = REST_Controller::HTTP_OK;
+        } else {
           $response['error'] = true;
           $response['message'] = "Status is not active. Please contact administration";
           $httpStatus = REST_Controller::HTTP_OK;
-        }  
+        }
       } else {
         $response['error'] = true;
         $response['message'] = "Login unscuccesfull. Please try again!";
         $httpStatus = REST_Controller::HTTP_OK;
       }
-      
-    }else{
+    } else {
       $response['error'] = true;
       $response['message'] = "Parameters not found";
       $httpStatus = REST_Controller::HTTP_BAD_REQUEST;
     }
-    
+
     $this->response($response, $httpStatus);
-    } 
+  }
 }
--- application/models/LoginModel.php
+++ application/models/LoginModel.php
@@ -1,37 +1,44 @@
 <?php
 
-class LoginModel extends CI_model {
+class LoginModel extends CI_model
+{
 
-	public function __construct() 
-    {
-        parent::__construct(); 
-        $this->load->database();
-    }
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+	}
 
 	/* Query for validation */
-    public function validate_login($email,$password) {
-    	$q = $this->db->where(['email'=>$email, 'password'=>$password])
-    				  ->from('login')	
-    				  ->get();
+	public function validate_login($id, $password)
+	{
+		$q = $this->db->where(['id' => $id, 'password' => $password])
+			->from('login')
+			->get();
 
-    	if($q->num_rows()){
-    		return $q->row();
-       	}else{
-       		return FALSE;
-       	}
+		if ($q->num_rows()) {
+			return $q->row();
+		} else {
+			return FALSE;
+		}
 	}
-	
-	/* Query for getting userId if status is 1 */
-	public function get_uniqueId($email, $type) {
-		$sql = 'SELECT id FROM '.$type.' WHERE email="'.$email.'" AND status=1';
+
+	/* Query for return true if status is 1 */
+	public function get_userStatus($id, $type)
+	{
+		if ($type == 'parent') {
+			$type = 'student'; // TODO remove this login type in future 
+		}
+		$sql = 'SELECT status FROM ' . $type . ' WHERE id="' . $id . '"';
 		$query = $this->db->query($sql);
-		if($query->num_rows()){
-    		return $query->row()->id;
-       	}else{
-       		return FALSE;
-       	}
+		if ($query->num_rows()) {
+			if ($query->row()->status == 1) {
+				return TRUE;
+			} else {
+				return FALSE;
+			}
+		} else {
+			return FALSE;
+		}
 	}
 }
-
-
-?>
